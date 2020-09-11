6e14c39d46652453f3c2e0cb2c21ad1ffd691910
tyleretheridge <tylerjetheridge98@gmail.com>
2020-09-11 10:47:54 -0500

    add recursive reverse function

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,22 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # Need a case for empty list
+        if not node:
+            # Can't reverse empty list
+            return
+        # If at tail
+        if not node.next_node:
+            # Set tail to head
+            self.head = node
+            # Set prev ref to next
+            node.set_next(prev)
+            return
+        # Recursion
+        # Successive nodes in recursion will become prevs
+        # Save node.next_node to node_prime before updating node.next_node
+        node_prime = node.get_next()
+        # Update node.next_node = prev input
+        node.set_next(prev)
+        # Recursive function call
+        return self.reverse_list(node_prime, node)
